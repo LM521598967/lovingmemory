c0af7b505ae0769e2f3ccd8d87ffb92279651cda
lovingmemory <106819138+lovingmemory@users.noreply.github.com>
2022-06-16 13:53:15 +0100

    Updating files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
-const name = 'Janet Doe'
-const dates = 'July 29th 1958 - Jan 4th 2021'
+const name = 'Lyn Hunt'
+const dates = 'from somewhere to somewhere'
 
 export default {
   publicRuntimeConfig: {
