14834b05a82027be9df467cda9e9ef03f5b6669c
lovingmemory <106819138+lovingmemory@users.noreply.github.com>
2022-06-16 14:55:24 +0100

    Updating files

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -8,9 +8,9 @@
 <h1 class="title">Order of service</h1>
 <p><em>Tuesday 28th January 2021</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration later in the year when COVID-19 restrictions are relaxed.</p>
 <p class="highlight">Shim Sham, Flim Flam. A bit of important text that you want to stand out, such as any dressing requirements.</p>
-<h2>Church Service <span class="highlight">10:30am</span></h2>
+<h2>Church Service <span class="highlight">11:30am</span></h2>
 <ul class="fa-ulist">
-    <li class="fa-list fa-icon">Service starts at 10:30am at Church Address</li>
+    <li class="fa-list fa-icon">Service starts at 10:30am at Church</li>
     <li class="fa-list fa-icon">Entrance music: And Can It Be</li>
     <li class="fa-list fa-icon">Scripture reading</li>
     <li class="fa-list fa-icon">Welcome &amp; Opening Prayer</li>
@@ -20,9 +20,9 @@
     <li class="fa-list fa-icon">Short message</li>
     <li class="fa-list fa-icon">Prayer of Commendation</li>
     <li class="fa-list fa-icon">Closing prayer &amp; Grace blessing</li>
-    <li class="fa-list fa-icon">Exit music: New Wine</li>
+    <li class="fa-list fa-icon">Exit music: New Wine</li><li class="fa-list fa-icon">Something or other</li>
 </ul>
-<h2>Crematorium <span class="highlight">12:00pm</span></h2>
+<h2>Crematorium <span class="highlight">1:00pm</span></h2>
 <ul class="fa-ulist">
     <li class="fa-list fa-icon">Service starts at 12pm at Crematorium address</li>
     <li class="fa-list fa-icon">Entrance music: And Can It Be</li>
